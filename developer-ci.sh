d4546be61a136980a6fe8308b58b41295ecb5891
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2015-07-06 13:42:38 +0530

    Sniff OS and arch since the agents are a mix of 32 bit and 64 bit machines.

--- developer-ci.sh
+++ developer-ci.sh
@@ -7,8 +7,34 @@ rm -rf $HOME/.node
 mkdir -p $HOME/.node
 git_short_sha=$(git log -1 --format=%h)
 
+nvm_get_os() {
+  local NVM_UNAME
+  NVM_UNAME="$(uname -a)"
+  local NVM_OS
+  case "$NVM_UNAME" in
+    Linux\ *) NVM_OS=linux ;;
+    Darwin\ *) NVM_OS=darwin ;;
+    SunOS\ *) NVM_OS=sunos ;;
+    FreeBSD\ *) NVM_OS=freebsd ;;
+  esac
+  echo "$NVM_OS"
+}
+
+nvm_get_arch() {
+  local NVM_UNAME
+  NVM_UNAME="$(uname -a)"
+  local NVM_ARCH
+  case "$NVM_UNAME" in
+    *x86_64*) NVM_ARCH=x64 ;;
+    *i*86*) NVM_ARCH=x86 ;;
+    *) NVM_ARCH="$(uname -m)" ;;
+  esac
+  echo "$NVM_ARCH"
+}
+
+
 if [ -n "$GO_SERVER_URL" ]; then
-    curl --fail --silent http://nodejs.org/dist/v0.12.3/node-v0.12.3-linux-x64.tar.gz | tar --strip-components=1 -zx -C $HOME/.node
+    curl --fail --silent http://nodejs.org/dist/v0.12.3/node-v0.12.3-$(nvm_get_os)-$(nvm_get_arch).tar.gz | tar --strip-components=1 -zx -C $HOME/.node
 fi
 
 export PATH=$HOME/.node/bin:$PATH
