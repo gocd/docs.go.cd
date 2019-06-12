4cec25d211b0ab63769a0578f836f8bac789e399
Aravind SV <mail@arvindsv.com>
2019-06-12 12:52:30 -0400

    Fix navigation
    There is a change in behavior: Previous and next only go till the end of the current section. Not beyond.

--- static/javascripts/navigation/navigation.js
+++ static/javascripts/navigation/navigation.js
@@ -1,47 +1,24 @@
-$(document).ready(function () {
-    var pathname = getPathname();
-    var ul = $("nav[role='navigation'] ul").eq(0);
-
-    var allMenu = ul.find("li").find("a");
-    allMenu.splice(0, 2);
-
-    var indexOfCurrentMenu = findIndexOf(allMenu, pathname);
-    var prevRef = (pathname === "/" || indexOfCurrentMenu === 0) ? null : allMenu.eq(indexOfCurrentMenu - 1);
-    var nextRef = (indexOfCurrentMenu === allMenu.length - 1) ? null : allMenu.eq(indexOfCurrentMenu + 1);
-
-    updateNavigationLink($(".navigation-prev"), prevRef);
-    updateNavigationLink($(".navigation-next"), nextRef);
-
-});
-
-function getPathname() {
-    var currentHref = $(location).attr("href");
-    var element = $('<a>', {
-        href: currentHref
-    });
-
-    var pathname = element.prop('pathname');
-    if (currentHref.indexOf("localhost:1313") >= 0) {
-        return pathname;
+(function() {
+  var updateNavigationLink = function(linkToUpdate, refElem) {
+    if (refElem.length > 0) {
+      linkToUpdate.attr("href", refElem.attr("href"));
+      linkToUpdate.attr("aria-label", refElem.text());
+      linkToUpdate.css('visibility', 'visible');
     }
-    return pathname.replace("/" + element.prop('pathname').split("/")[1], "")
-}
+  };
 
-function updateNavigationLink(linkToUpdate, refElem) {
-    if (refElem) {
-        linkToUpdate.attr("href", refElem.attr("href"));
-        linkToUpdate.attr("aria-label", refElem.text());
-        linkToUpdate.css('visibility', 'visible');
-    } else {
-        linkToUpdate.css('visibility', 'hidden');
-    }
-}
+  $(document).ready(function () {
+    var currentPagePermalink = $("body").data("rel-permalink");
+    var currentPageLinksInMenu = $('.book-menu nav li > a[href$="' + currentPagePermalink + '"]');
 
-function findIndexOf(list, pathToFind) {
-    for (var i = 0; i < list.length; i++) {
-        if (list[i].getAttribute("href").indexOf(pathToFind) !== -1) {
-            return i;
-        }
+    var prevPageLinkElement = $(".navigation-prev").css('visibility', 'hidden');
+    var nextPageLinkElement = $(".navigation-next").css('visibility', 'hidden');
+
+    if (currentPagePermalink === "" || currentPagePermalink === "/" || currentPageLinksInMenu.length <= 0) {
+      return;
     }
-    return -1;
-}
+
+    updateNavigationLink(prevPageLinkElement, currentPageLinksInMenu.parent("li").prev("li").find("a"));
+    updateNavigationLink(nextPageLinkElement, currentPageLinksInMenu.parent("li").next("li").find("a"));
+  });
+})();
