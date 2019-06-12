f90875446766aeb6373deae948f05adc85ff1dae
Aravind SV <mail@arvindsv.com>
2019-06-12 15:14:48 -0400

    Fix navigation so that it goes across sections

--- static/javascripts/navigation/navigation.js
+++ static/javascripts/navigation/navigation.js
@@ -1,24 +1,38 @@
 (function() {
-  var updateNavigationLink = function(linkToUpdate, refElem) {
-    if (refElem.length > 0) {
-      linkToUpdate.attr("href", refElem.attr("href"));
-      linkToUpdate.attr("aria-label", refElem.text());
-      linkToUpdate.css('visibility', 'visible');
+  var updateNavigationLink = function(linkToUpdate, possibleChoicesForNavigation) {
+    for (var choiceFnIndex in possibleChoicesForNavigation) {
+      var choice = possibleChoicesForNavigation[choiceFnIndex]();
+      if (choice.length > 0) {
+        linkToUpdate.attr("href", choice.attr("href"));
+        linkToUpdate.attr("aria-label", choice.text());
+        linkToUpdate.attr("title", choice.text());
+        linkToUpdate.css('visibility', 'visible');
+        return;
+      }
     }
   };
 
   $(document).ready(function () {
     var currentPagePermalink = $("body").data("rel-permalink");
-    var currentPageLinksInMenu = $('.book-menu nav li > a[href$="' + currentPagePermalink + '"]');
+    var currentPageLinkInMenu = $('.book-menu nav li > a[href$="' + currentPagePermalink + '"]').parent("li").first();
 
-    var prevPageLinkElement = $(".navigation-prev").css('visibility', 'hidden');
-    var nextPageLinkElement = $(".navigation-next").css('visibility', 'hidden');
+    var prevPageLinkElement = $(".navigation-prev");
+    var nextPageLinkElement = $(".navigation-next");
 
-    if (currentPagePermalink === "" || currentPagePermalink === "/" || currentPageLinksInMenu.length <= 0) {
+    if (currentPagePermalink === "" || currentPagePermalink === "/" || currentPageLinkInMenu.length <= 0) {
       return;
     }
 
-    updateNavigationLink(prevPageLinkElement, currentPageLinksInMenu.parent("li").prev("li").find("a"));
-    updateNavigationLink(nextPageLinkElement, currentPageLinksInMenu.parent("li").next("li").find("a"));
+    updateNavigationLink(prevPageLinkElement, [
+      () => currentPageLinkInMenu.prev("li").find("a[href!='#']").last(),                       // Previous sibling
+      () => currentPageLinkInMenu.parents("li").first().children("a[href!='#']"),               // A parent with a link
+      () => currentPageLinkInMenu.parents("li").first().prev("li").find("a[href!='#']").last(), // Last element of previous section.
+    ]);
+
+    updateNavigationLink(nextPageLinkElement, [
+      () => currentPageLinkInMenu.find("li a[href!='#']").first(),                              // A child
+      () => currentPageLinkInMenu.next("li").find("a[href!='#']").first(),                      // Next sibling
+      () => currentPageLinkInMenu.parents("li").first().next("li").find("a[href!='#']").first() // First element of next section.
+    ]);
   });
 })();
