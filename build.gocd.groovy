4ea714467d1f6b5bb7afac11d010282a51fa3bb9
Kritika Singh <kritika.singh1994@gmail.com>
2019-07-22 15:31:34 +0530

    Trying out ubuntu-16.04 as elastic profile id

--- build.gocd.groovy
+++ build.gocd.groovy
@@ -14,7 +14,7 @@ def buildStage = {
         cleanWorkingDir = true
         jobs {
             job("BuildWebsite") {
-                elasticProfileId = 'ecs-gocd-dev-build'
+                elasticProfileId = 'ubuntu-16.04'
                 tasks {
                     bash {
                         commandString = "bundle install --path .bundle --jobs 4"
@@ -33,7 +33,7 @@ def pushToGHPages = {
         cleanWorkingDir = true
         jobs {
             job("PushToGHPages") {
-                elasticProfileId = 'ecs-gocd-dev-build'
+                elasticProfileId = 'ubuntu-16.04'
                 tasks {
                     bash {
                         commandString = "git remote add upstream 'https://\${BUILD_MAP_USER}:\${BUILD_MAP_PASSWORD}@github.com/gocd/docs.go.cd'"
@@ -55,7 +55,7 @@ def publishToS3 = {
         cleanWorkingDir = true    
         jobs {
             job("upload") {
-                elasticProfileId = 'ecs-gocd-dev-build'
+                elasticProfileId = 'ubuntu-16.04'
                 tasks {
                     bash {
                         commandString = "bundle install --path .bundle --jobs 4"
